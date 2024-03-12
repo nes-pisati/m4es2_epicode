aa61f489cfeb79956e82e9f86ca49d2c08738db1
Vanessa Pisati <pisati.vanessa@gmail.com>
2024-03-12 20:53:06 +0100

    remake

--- HW2/js/script.js
+++ HW2/js/script.js
@@ -1,129 +1,136 @@
-/*function createCard() {
+const dialog = document.querySelector("dialog");
+const myPlaylist = [];
+const playlistBtn = document.getElementById("myPlaylist");
+let savedPlaylist = JSON.parse(localStorage.getItem("playlist"));
+const closeButton = document.querySelector("dialog button");
+const clearPlaylist = document.getElementById("clearPlaylist");
+const dialogBody = document.querySelector("#dialogBody")
 
-    const card = document.createElement("div");
-    card.classList = "album-card card col-lg-3 col-6 p-2 d-flex align-items-center";
-    const cardImage = document.createElement("img");
-    cardImage.src = "https://picsum.photos/200";
-    cardImage.classList = "card-image";
-    const albumTitle = document.createElement("p");
-    albumTitle.innerText = "Lorem Ipsum";
-    albumTitle.classList = "album-title mt-2 mb-2";
-    const titleButton = document.createElement("button");
-    titleButton.innerText = "Track List"
-    titleButton.id = "title-button";
-    titleButton.classList = "title-button mb-1";
+closeButton.addEventListener("click", ()=> {
+    dialogBody.innerHTML = "";
+    dialog.close();
+})
 
-    card.append(cardImage, albumTitle, titleButton)
-    document.querySelector("#searchResults").appendChild(card);
+playlistBtn.addEventListener("click", ()=> {
+    
+    dialog.showModal()
+    if (savedPlaylist != null) {
+        savedPlaylist.map((item) => populateDialog(item));
+    }
+})
+
+clearPlaylist.addEventListener("click", ()=> {
+    localStorage.clear()
+    dialogBody.innerHTML = "";
+})
+
+function populateDialog(song){
+    const createParagraph = document.createElement("p");
+    createParagraph.innerHTML = `${song}`;
+    dialogBody.append(createParagraph);
 }
-
-createCard()*/
-
-//TEST PER CICLARE ARRAY E VERIFICARE SE LA FUZIONE createCard() venisse eseguita per ogni elemento dell'array.
-
-/*let arrayTest = [1,2,3,4]
-
-for (let i=0; i<arrayTest.length; i++) {
-    createCard()
-}*/
 
 function createWrapper() {
     const resultWrapper = document.createElement("div");
-    resultWrapper.classList = "result-wrapper d-flex justify-content-between"
+    resultWrapper.classList = "result-wrapper d-flex justify-content-between flex-wrap g-2"
     document.querySelector("#searchResults").appendChild(resultWrapper);
 }
 
 createWrapper()
 
-function showData(serverResponse) {
+function showData(image, title,) {
 
     //TEST 
-    console.log(serverResponse); //restituisce oggetto contenente array di oggetti
+    /*console.log(serverResponse); //restituisce oggetto contenente array di oggetti
     console.log(serverResponse.data); //restituisce array di oggetti
-    console.log(serverResponse.data[1].title);
-
-    //TEST STAMPA IN HTML
-    /*const test = document.querySelector(".test");
-    test.innerText = serverResponse.data[3].title;*/  
+    console.log(serverResponse.data[1].title);*/
     
     const card = document.createElement("div");
     card.classList = "album-card card col-lg-3 col-6 p-2 d-flex align-items-center";
     const cardImage = document.createElement("img");
-    cardImage.src = serverResponse.data[2].album.cover
+    cardImage.src = image;
     cardImage.classList = "card-image";
     const albumTitle = document.createElement("p");
-    albumTitle.innerText = serverResponse.data[2].title;
+    albumTitle.innerText = title;
     albumTitle.classList = "album-title mt-2 mb-2";
     const titleButton = document.createElement("button");
-    titleButton.innerText = "Track List"
+    titleButton.innerText = "Add to Playlist"
     titleButton.id = "title-button";
     titleButton.classList = "title-button mb-1";
+    titleButton.addEventListener("click", ()=> {
+        savePlaylistToLocalStorage(title, myPlaylist);
+    })
 
     card.append(cardImage, albumTitle, titleButton)
     document.querySelector(".result-wrapper").appendChild(card);
-
-    /*for (let i = 0; i < serverResponse.data.lenght; i++) {
-        const card = document.createElement("div");
-        card.classList = "album-card card col-3 p-2 d-flex align-items-center";
-        const cardImage = document.createElement("img");
-        cardImage.src = serverResponse.data[i].cover;
-        cardImage.classList = "card-image";
-        const albumTitle = document.createElement("p");
-        albumTitle.innerText = serverResponse.data[i].album.title;
-        albumTitle.classList = "album-title mt-2 mb-2";
-        const titleButton = document.createElement("button");
-        titleButton.innerText = "Track List"
-        titleButton.id = "title-button";
-        titleButton.classList = "title-button mb-1";
-
-        card.append(cardImage, albumTitle, titleButton)
-        document.querySelector("#searchResults").appendChild(card);
-    }*/
 }
 
+console.log(myPlaylist);
+
+function savePlaylistToLocalStorage(song, array) {
+    array.push(song);
+    localStorage.setItem("playlist", JSON.stringify(array))
+}
 
 //Eminem
 
-fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+const getAlbums = async ()=> {
+    try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=eminem")
+        const data = await response.json();
 
-.then(function(eminemResponse) {
+        return data.data
+    } catch (error) {
+        console.log("Errore durante la richiesta degli album");
+    }
+}
+
+getAlbums().then(albums => {
+    albums.map((album) => {
+        //console.log(album);
+        showData(album.album.cover_medium, album.title)
+    })
+})
+
+/*.then(function(eminemResponse) {
     console.log(eminemResponse);
 
     return eminemResponse.json();
 })
 
 .then(function(eminemData) {
-    console.log(eminemData);
-    console.log(eminemData.data[0].title);
+    //console.log(eminemData);
+    //console.log(eminemData.data[0].title);
+
     showData(eminemData);
-})
+})*/
 
 //Metallica
 
 fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica")
 
-.then(function(metallicaResponse) {
-    console.log(metallicaResponse);
+/*.then(function(metallicaResponse) {
+    //console.log(metallicaResponse);
 
     return metallicaResponse.json();
 })
 
 .then(function(metallicaData) {
-    console.log(metallicaData);
+    //console.log(metallicaData);
     showData(metallicaData)
-})
+})*/
 
 //Queen
 
 fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=queen")
 
-.then(function(queenResponse) {
-    console.log(queenResponse);
+/*.then(function(queenResponse) {
+    //console.log(queenResponse);
 
     return queenResponse.json();
 })
 
 .then(function(queenData) {
-    console.log(queenData);
+    //console.log(queenData);
     showData(queenData)
-})
+})*/
